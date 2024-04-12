ccf60776a3c17061754fce6badbd0a0896b43a56
Hari haran <hariharandakshnamurthy@gmail.com>
2024-04-13 00:44:56 +0530

    commit

---
+++ day1.py
@@ -0,0 +1,62 @@
+print("Hello world..!!!!")
+
+""" Problem 1 Open the python interactive shell and do the following operations.
+ The operands are 3 and 4.
+addition(+)
+subtraction(-)
+multiplication(*)
+modulus(%)
+division(/)
+exponential(**)
+floor division operator(//) """
+
+a=3
+b=4
+#addition
+c=a+b
+print("addition: ",c);
+#subtraction
+c=a-b
+print("subtraction: ",c);
+#multiplication
+c=a*b
+print("multiplication: ",c);
+#division
+c=a/b
+print("division: ",c);
+#exponentiation
+c=a**b
+print("exponential: ",c);
+#floor division
+c=a//b
+print("floor div: ",c);
+
+""" Write strings on the python interactive shell. The strings are the following:
+Your name
+Your family name
+Your country
+I am enjoying 30 days of python """
+
+print("My name is Hariharan Dakshnamurthy");
+print("My Family name is Gojo");
+print("My country is India");
+print('I am enjoying 30 days of python');
+
+""" Check the data types of the following data:
+10
+9.8
+3.14
+4 - 4j
+['Asabeneh', 'Python', 'Finland']
+Your name
+Your family name
+Your country """
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Your name'))
+print(type('Your family name'))
+print(type('Your country '))
---
+++ distance.py
@@ -0,0 +1,8 @@
+import math
+""" Find an Euclidian distance between (2, 3) and (10, 8) """
+x1 = 2
+x2 = 10
+y1=3
+y2=8
+distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+print('Euclidian distance between (2, 3) and (10, 8) is ',distance)
